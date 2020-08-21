302127e6d82785c337c4bb19d5cb0b195cbdd30a
Nicolas BAUW <nbauw@hotmail.com>
2020-08-21 21:21:12 +0200

    unset function

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 [![Current Crates.io Version](https://img.shields.io/crates/v/globalenv.svg)](https://crates.io/crates/globalenv)
 [![Downloads badge](https://img.shields.io/crates/d/globalenv.svg)](https://crates.io/crates/globalenv)
 
-Globally sets environment variables (and not just for the current process).
+Globally set or unset environment variables (and not just for the current process).
 Example:
 ```rust
 use globalenv::set_var;
 set_var("ENVTEST", "TESTVALUE").unwrap();
+unset_var("ENVTEST").unwrap();
 ```
 
 License: GPL-3.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-//! Globally sets environment variables (and not just for the current process).
+//! Globally set or unset environment variables (and not just for the current process).
 //! Example:
 //! ```rust
-//! use globalenv::set_var;
+//! use globalenv::{set_var, unset_var};
 //! set_var("ENVTEST", "TESTVALUE").unwrap();
+//! unset_var("ENVTEST").unwrap();
 //! ```
 
 use std::io;
@@ -14,6 +15,14 @@ pub fn set_var(var: &str, value: &str) -> io::Result<()> {
     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
     let key = hkcu.open_subkey_with_flags("Environment", KEY_SET_VALUE)?;
     key.set_value(var, &value)?;
+    Ok(())
+}
+
+/// Unsets an environment variable. Support for Windows. Linux support TBD.
+pub fn unset_var(var: &str) -> io::Result<()> {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let key = hkcu.open_subkey_with_flags("Environment", KEY_SET_VALUE)?;
+    key.delete_value(var)?;
     Ok(())
 }
 
@@ -30,9 +39,16 @@ mod tests {
             .unwrap();
         let var: String = key.get_value("ENVTEST").unwrap();
         assert_eq!(String::from("TESTVALUE"), var);
+    }
+
+    #[test]
+    #[should_panic]
+    fn unset() {
+        crate::unset_var("ENVTEST").unwrap();
+        let hkcu = RegKey::predef(HKEY_CURRENT_USER);
         let key = hkcu
-            .open_subkey_with_flags("Environment", KEY_SET_VALUE)
+            .open_subkey_with_flags("Environment", KEY_READ)
             .unwrap();
-        key.delete_value("ENVTEST").unwrap();
+        let _: String = key.get_value("ENVTEST").unwrap();
     }
 }
